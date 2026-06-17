c922f0d792e53c9d52917569dbf17f5bc390eedb
CJanithWaduge <chamudithawaduge2346@gmail.com>
2026-06-17 17:46:14 +0530

    Fix double browser open - disable reloader

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     print("Starting Image Toolkit...")
     print("Open http://127.0.0.1:8000 in your browser.")
     webbrowser.open("http://127.0.0.1:8000")
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=True, use_reloader=False)
